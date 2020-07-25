18b91054b6a12f130c0a906f30b61556b7c0756c
Jason Nelson <Jasonn318@gmail.com>
2020-07-25 13:38:11 -0500

    added PostController component, adjusted CSS in Post, Sidebar, and HomeController

--- src/Components/Post/Post.tsx
+++ src/Components/Post/Post.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Post.module.css';
+import PostController from './PostController/PostController';
 
 const PostsFeed = (props: any) => {
   let mockPosts = [
@@ -44,12 +45,15 @@ const PostsFeed = (props: any) => {
         <div className={styles.PostHead}>
           <div className={styles.UserInfo}>
             <h1 className={styles.Username}>{post.userName}</h1>
-  <p className={styles.Handle}>{post.handle}</p>
-  <p className={styles.TimeTweeted}>{post.date.toDateString()}</p>
+            <p className={styles.Handle}>{post.handle}</p>
+            <p className={styles.TimeTweeted}>{post.date.toDateString()}</p>
           </div>
           <svg viewBox="0 0 24 24" className={styles.DownArrow}><g><path d="M20.207 8.147c-.39-.39-1.023-.39-1.414 0L12 14.94 5.207 8.147c-.39-.39-1.023-.39-1.414 0-.39.39-.39 1.023 0 1.414l7.5 7.5c.195.196.45.294.707.294s.512-.098.707-.293l7.5-7.5c.39-.39.39-1.022 0-1.413z"></path></g></svg>
         </div>
-  <p className={styles.PostContent}>{post.tweetText}</p>
+        <p className={styles.PostContent}>{post.tweetText}</p>
+        <div className={styles.PostController}>
+          <PostController></PostController>
+        </div>
       </div>
     </div>
   ));
